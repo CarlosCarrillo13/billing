99b25a04effbf088fcff56fb70f838520d7b6455
Carlos Carrillo <carrilloduque.carlos@gmail.com>
2019-04-11 21:42:57 -0500

    first commit

---
+++ billingsource/src/main/java/com/carrillo/billingsource/billingsource/BillingsourceApplication.java
@@ -0,0 +1,13 @@
+package com.carrillo.billingsource.billingsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingsourceApplication.class, args);
+	}
+
+}
---
+++ billingsource/src/test/java/com/carrillo/billingsource/billingsource/BillingsourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carrillo.billingsource.billingsource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillingsourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
